134520e3dd538218ad0cb7c016110433b6615a20
Caitlin Potter <snowball@defpixel.com>
2013-06-17 17:21:12 -0400

    First-edition of Unlock Pi-script, using webservice
    This first version has a few obvious issues:
    
    1) Constant hostname for webservice
    2) Not sure if this kind of calling system is the one we
       want to use
    3) Obviously, the console.log output is not going to look like
       that forever. It's just there to make debugging easier without
       a proper test unit.
    
    The idea is that the RFID reader should trigger this script to
    execute when a card is swiped, and it should pass 'type=rfid' and
    'id=<the parsed ID of the RFID card>', which will then get passed
    off to the webservice who decides whether or not we want to unlock
    the door. So most of the hard work happens on the web service.
    
    As far as automated testing is concerned... We can certainly test
    that the client gets an expected response from the web service on
    a regular basis. But I think I will be writing this test in a
    subsequent patch.

---
+++ Pi_Scripts/unlock.js
@@ -0,0 +1,44 @@
+var restify = require('restify');
+
+// TODO:
+// Probably want some configuration file that points to the
+// webservice, since we can't expect it to be localhost:8080
+// all the time.
+var client = restify.createJsonClient({
+  url: 'http://localhost:8080',
+  version: '~1.0'
+});
+
+var type, id;
+
+process.argv.forEach(function(val,index,array){
+  if(index>1) {
+  	var s = new String(val)
+  	if(s.indexOf('type=') === 0){
+  		type = s.substr(5)
+  	} else if(s.indexOf('id=') === 0) {
+		  id = s.substr(3)
+  	} else {
+      console.log("Unsupported option `" + s + "'")
+    }
+  }
+})
+
+client.get('/auth/'+type+'/'+id, function (err, req, res, obj) {
+  if(err) console.log(err)
+  else {
+    try {
+      if(obj.auth===true) {
+      	// TODO:
+      	// Send GPIO signal to open door
+      	console.log("Open the hatch!")
+      } else {
+      	console.log("Can't open the hatch, sorry :(")
+      }
+    } catch(e) {
+  	  console.log("Object does not contain 'auth' parameter! Did something change?")
+  	  console.log(obj)
+    }
+  }
+  client.close();
+});
