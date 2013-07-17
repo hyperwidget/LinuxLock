f340fa35fc86834e4207f0f01352a905915a061e
Caitlin Potter <snowball@defpixel.com>
2013-07-17 11:19:10 -0400

    Filter from "Today" until "Now" in Reports by default
    Maybe this isn't desirable, but it does at least limit the traffic
    and save some time.

--- Client_Code/js/controllers/ReportsController.js
+++ Client_Code/js/controllers/ReportsController.js
@@ -8,6 +8,13 @@ adminConsoleApp.controller('ReportsController',
     $scope.totalEntries = 0
     $scope.updateReport = function(filter) {
       // No pagination yet :(
+      if(filter) {
+        if("from" in filter) $scope.from = filter.from
+        if("to" in filter) $scope.to = filter.to
+        if("who" in filter) $scope.who = filter.who
+        if("dev" in filter) $scope.dev = filter.dev
+        if("rfid" in filter) $scope.rfid = filter.rfid
+      }
       var params = {
         rfid: $scope.rfid,
         dev: $scope.dev,
@@ -17,6 +24,6 @@ adminConsoleApp.controller('ReportsController',
       }
       $scope.events = dataManager.Event.query(params)
     }
-    $scope.updateReport()
+    $scope.updateReport({from: "Today", to: "Now"})
   }
 );
