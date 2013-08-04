967cb0847e44f4ca3bfb1b77a1d240045e8bf0bb
daag <fattonyd@hotmail.com>
2013-08-03 22:51:09 -0400

    Fixes #25 Added functionality to check whether zones exist in a cardholder or a device exists in a zone

--- Client_Code/js/controllers/DevicesController.js
+++ Client_Code/js/controllers/DevicesController.js
@@ -23,9 +23,19 @@ adminConsoleApp.controller('DevicesController',
         $scope.deleteDevice = function() {
             $scope.confirm = confirm('Are you sure you want to delete this device?');
             if($scope.confirm === true){
-                $scope.currentDevice = $scope.devices[$scope.currentIndex];
-                $scope.currentDevice.$delete();
-                $scope.devices = dataManager.Device.query();
+                dataManager.Zone.query({device: $scope.devices[$scope.currentIndex]._id}, function(data) {
+                    if (data.length > 0) {
+                        if (confirm('The device you are about to delete is still a part of 1 or more zones. Do you wish to delete anyway?')) {
+                            $scope.currentDevice = $scope.devices[$scope.currentIndex];
+                            $scope.currentDevice.$delete();
+                            $scope.devices = dataManager.Device.query();
+                        }
+                    } else {
+                        $scope.currentDevice = $scope.devices[$scope.currentIndex];
+                        $scope.currentDevice.$delete();
+                        $scope.devices = dataManager.Device.query();
+                    }
+                });
             }
         };
         $scope.changeCurrentDevice = function (event, index) {
